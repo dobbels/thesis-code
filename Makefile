15f30827d6f6a08d72bdd809d40c90a8e3248b7d
Instant Contiki <steven.dobbelaere@hotmail.com>
2019-02-17 17:56:21 +0100

    add hidra file skeleton

---
+++ Makefile
@@ -0,0 +1,7 @@
+CONTIKI_PROJECT = hidra-r 
+ all: $(CONTIKI_PROJECT)
+
+ #UIP_CONF_IPV6=1
+
+ CONTIKI = contiki
+ include $(CONTIKI)/Makefile.include
---
+++ hidra-r.c
@@ -0,0 +1,18 @@
+/*<Header Files>*/
+#include "contiki.h"
+#include <stdio.h>
+
+PROCESS(hidra_r,"HidraR");
+AUTOSTART_PROCESSES(&hidra_r);
+
+PROCESS_THREAD(hidra_r, ev, data)
+{ 
+	PROCESS_BEGIN();
+	
+	
+	
+	
+	
+	PROCESS_END();
+}
+
